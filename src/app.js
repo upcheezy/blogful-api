69e67429d4080d7c12601755185d58d201b851d7
Matthew Upchurch <upcheezy@gmail.com>
2020-05-12 22:23:19 -0400

    first commit

---
+++ src/app.js
@@ -0,0 +1,93 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const {
+    NODE_ENV
+} = require('./config')
+const ArticlesService = require('./articles-service');
+
+const app = express()
+const jsonParser = express.json()
+
+const morganOption = (NODE_ENV === 'production') ?
+    'tiny' :
+    'common';
+
+app.use(morgan(morganOption))
+app.use(helmet())
+app.use(cors())
+
+app.get('/articles', (req, res, next) => {
+    const knexInstance = req.app.get('db');
+    ArticlesService.getAllArticles(knexInstance)
+        .then(articles => {
+            res.json(articles)
+        })
+        .catch(next);
+})
+
+app.post('/articles', jsonParser, (req, res, next) => {
+    const {
+        title,
+        content,
+        style
+    } = req.body;
+    const newArticle = {
+        title,
+        content,
+        style
+    };
+    ArticlesService.insertArticle(
+            req.app.get('db'),
+            newArticle
+        )
+        .then(article => {
+            res
+                .status(201)
+                .location(`/articles/${article.id}`)
+                .json(article)
+        })
+        .catch(next)
+})
+
+app.get('/articles/:article_id', (req, res, next) => {
+    const knexInstance = req.app.get('db');
+    ArticlesService.getById(knexInstance, req.params.article_id)
+        .then(article => {
+            if (!article) {
+                return res.status(404).json({
+                    error: {
+                        message: `Article doesn't exist`
+                    }
+                })
+            }
+            res.json(article)
+        })
+        .catch(next)
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!')
+})
+
+app.use(function errorHandler(error, req, res, next) {
+    let response;
+    if (NODE_ENV === 'production') {
+        response = {
+            error: {
+                message: 'server error'
+            }
+        }
+    } else {
+        console.error(error);
+        response = {
+            message: error.message,
+            error
+        }
+    }
+    res.status(500).json(response);
+})
+
+module.exports = app
---
+++ test/articles-endpoints.spec.js
@@ -0,0 +1,116 @@
+const {
+    expect
+} = require('chai');
+const knex = require('knex');
+const app = require('../src/app')
+const {
+    makeArticlesArray
+} = require('./articles.fixtures')
+
+describe.only('Articles Endpoints', function () {
+    let db
+
+    before('make knex instance', () => {
+        db = knex({
+            client: 'pg',
+            connection: process.env.TEST_DB_URL,
+        })
+        app.set('db', db);
+    })
+
+    after('disconnect from db', () => db.destroy())
+
+    before('clean the table', () => db('blogful_articles').truncate())
+
+    afterEach('cleanup', () => db('blogful_articles').truncate())
+
+    describe(`GET /articles`, () => {
+        context('Given there are articles in the database', () => {
+            const testArticles = makeArticlesArray()
+
+            beforeEach('insert articles', () => {
+                return db
+                    .into('blogful_articles')
+                    .insert(testArticles)
+            })
+
+            it('responds with 200 and all of the articles', () => {
+                return supertest(app)
+                    .get('/articles')
+                    .expect(200, testArticles)
+            })
+        })
+
+        context('Given no articles', () => {
+            it(`responds with 200 and an empty list`, () => {
+                return supertest(app)
+                    .get('/articles')
+                    .expect(200, [])
+            })
+        })
+    })
+
+    describe(`GET /articles/:article_id`, () => {
+        context('Given there are articles in the database', () => {
+            const testArticles = makeArticlesArray()
+
+            beforeEach('insert articles', () => {
+                return db
+                    .into('blogful_articles')
+                    .insert(testArticles)
+            })
+
+            it('responds with 200 and the specified article', () => {
+                const articleId = 2
+                const expectedArticle = testArticles[articleId - 1]
+                return supertest(app)
+                    .get(`/articles/${articleId}`)
+                    .expect(200, expectedArticle)
+            })
+
+            describe.only(`POST /articles`, () => {
+                it(`creates an article, responding with 201 and the new article`, function () {
+                    const newArticle = {
+                        title: 'Test new article',
+                        style: 'Listicle',
+                        content: 'Test new article content...'
+                    }
+                    return supertest(app)
+                        .post('/articles')
+                        .send(newArticle)
+                        .expect(201)
+                        .expect(res => {
+                            expect(res.body.title).to.eql(newArticle.title)
+                            expect(res.body.style).to.eql(newArticle.style)
+                            expect(res.body.content).to.eql(newArticle.content)
+                            expect(res.body).to.have.property('id')
+                            expect(res.headers.location).to.eql(`/articles/${res.body.id}`)
+                            const expected = new Date().toLocaleString('en', { timeZone: 'UTC' })
+                            const actual = new Date(res.body.date_published).toLocaleString()
+                            expect(actual).to.eql(expected)
+                        })
+                        .then(postRes =>
+                            supertest(app)
+                            .get(`/articles/${postRes.body.id}`)
+                            .expect(postRes.body)
+                        )
+                })
+            })
+        })
+
+        context(`Given no articles`, () => {
+            context(`Given no articles`, () => {
+                it(`responds with 404`, () => {
+                    const articleId = 123456;
+                    return supertest(app)
+                        .get(`/articles/${articleId}`)
+                        .expect(404, {
+                            error: {
+                                message: `Article doesn't exist`
+                            }
+                        })
+                })
+            })
+        })
+    })
+})
